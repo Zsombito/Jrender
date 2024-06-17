Fixed gradients and added an example

jrenderer/lights.py
@@ -16,7 +16,7 @@ class Light:
         - point: 0 = directional, 1 = point
         """
         self.intensity = jnp.array(intenisity)
-        self.pos = jnp.append(jnp.array(position), jnp.array(1), 0)
+        self.pos = jnp.append(jnp.array(position), jnp.array([1]), 0)
         self.point = point
     
     def getJnpArray(self):